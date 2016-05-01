54a1de65c7e6a86c71c3ec15a2c460339e4f850f
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2016-05-02 00:55:27 +0300

    Added index.html and timer.js.
    Prepare templates function.

---
+++ js/timer.js
@@ -0,0 +1,52 @@
+// vlad kovaliov
+// 1 may 2016
+// timer on canvas
+
+// Constructor
+function Timer() {
+
+};
+
+Timer.prototype.init = function() {
+
+};
+
+Timer.prototype.play = function() {
+
+};
+
+Timer.prototype.stop = function() {
+
+};
+
+Timer.prototype.updateDiscrete = function() {
+
+};
+
+Timer.prototype.updateContinues = function() {
+
+};
+
+Timer.prototype.drawDays = function() {
+
+};
+
+Timer.prototype.drawHours = function() {
+
+};
+
+Timer.prototype.drawMinutes = function() {
+
+};
+
+Timer.prototype.drawSeconds = function() {
+
+};
+
+Timer.prototype.setValue = function() {
+
+};
+
+Timer.prototype.isOptions = function() {
+
+};
